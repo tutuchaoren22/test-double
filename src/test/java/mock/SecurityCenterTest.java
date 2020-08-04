42476b33dc2aee82afafb43571c93b757c017462
duyuqian <809009017@qq.com>
2020-08-04 12:01:01 +0800

    [duyuqian] refactor: refactor mock test use mockito

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -4,6 +4,8 @@ package mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -11,17 +13,20 @@ public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
+    @InjectMocks
     private SecurityCenter securityCenter;
-    private MockDoorPanel mockDoorPanel;
+
+    @Mock
+    private DoorPanel mockDoorPanel;
+
     @BeforeEach
     public void setUp() {
-        mockDoorPanel= new MockDoorPanel();
+        mockDoorPanel= new DoorPanel();
         securityCenter = new SecurityCenter(mockDoorPanel);
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
         securityCenter.switchOn();
-        assertTrue(mockDoorPanel.isHasCalled());
     }
 }
